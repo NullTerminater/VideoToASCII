d437d664e8d32e23df6f66e14ada9c31e6170a1f
Javidan <javidanaghayev1@gmail.com>
2026-02-10 22:33:08 -0600

    A somewhat poor way of recording a two second video, but it is progress i guess

--- video_to_ascii.c
+++ video_to_ascii.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
-
+#include <libavdevice/avdevice.h>
+#include <libavformat/avformat.h>
 
 int main(void) {
+    const char *cmd = "ffmpeg -f avfoundation -video_size 640x480 -framerate 30 -i \"0\" -vframes 60 test.mp4";
+    system(cmd);
 
+    // avdevice_register_all();
+
+    // AVInputFormat *fmt = av_find_input_format("avfoundation");
+
+    // AVFormatContext *ctx = NULL;
+    // if (avformat_open_input(&ctx, "0", fmt, NULL) != 0) {
+    //     // Handle error
+    //     puts("somethings trucked");
+    // }
 
     return 0;
 }
