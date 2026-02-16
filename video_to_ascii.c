43b0a845b8a7739f5c8c4254c55eb227c93231cd
Javidan <javidanaghayev1@gmail.com>
2026-02-15 22:32:43 -0600

    reading from a single frame

--- video_to_ascii.c
+++ video_to_ascii.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <libavdevice/avdevice.h>
 #include <libavformat/avformat.h>
+#include <stdint.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 
 int main(void) {
-    const char *brightness = " .:-=+*#%@";
+    // const char *brightness = " .:-=+*#%@";
     const char *cmd = "ffmpeg -f avfoundation -video_size 640x480 -framerate 30 -i \"0\" -vf \"format=gray\" -vframes 60 test.mp4";
     system(cmd);
     const char *extract = "ffmpeg -i test.mp4 -r 1 frame%d.bmp";
     system(extract);
+    int width, height, bpp;
+
+    uint8_t* rgb_image = stbi_load("frame1.bmp", &width, &height, &bpp, 3);
+    printf("properties %s \n", rgb_image);
+    stbi_image_free(rgb_image);
 
     // avdevice_register_all();
 
