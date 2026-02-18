79215b92324bd08acaf58da841b1883cb9531088
Javidan <javidanaghayev1@gmail.com>
2026-02-17 21:34:28 -0600

    Access the properties of the bpm image

--- video_to_ascii.c
+++ video_to_ascii.c
@@ -8,25 +8,16 @@
 
 int main(void) {
     // const char *brightness = " .:-=+*#%@";
-    const char *cmd = "ffmpeg -f avfoundation -video_size 640x480 -framerate 30 -i \"0\" -vf \"format=gray\" -vframes 60 test.mp4";
-    system(cmd);
-    const char *extract = "ffmpeg -i test.mp4 -r 1 frame%d.bmp";
-    system(extract);
+
+    // const char *cmd = "ffmpeg -f avfoundation -video_size 640x480 -framerate 30 -i \"0\" -vf \"format=gray\" -vframes 60 test.mp4";
+    // system(cmd);
+    // const char *extract = "ffmpeg -i test.mp4 -r 1 frame%d.bmp";
+    // system(extract);
     int width, height, bpp;
 
     uint8_t* rgb_image = stbi_load("frame1.bmp", &width, &height, &bpp, 3);
-    printf("properties %s \n", rgb_image);
+    printf("properties %d %d %d \n", width, height, bpp);
     stbi_image_free(rgb_image);
-
-    // avdevice_register_all();
-
-    // AVInputFormat *fmt = av_find_input_format("avfoundation");
-
-    // AVFormatContext *ctx = NULL;
-    // if (avformat_open_input(&ctx, "0", fmt, NULL) != 0) {
-    //     // Handle error
-    //     puts("somethings trucked");
-    // }
 
     return 0;
 }
